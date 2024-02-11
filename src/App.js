b9a41dc8b929d3d9d94fc3222c5ccc113f9bc081
Dev Patel <95192549+DevP25@users.noreply.github.com>
2024-02-11 10:23:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,8 @@ import './App.css';
 import Homepage from './components/Homepage';
 import Report from './components/Report';
 
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-
-const libraries = ['places'];
-const mapContainerStyle = {
-  width: '50vw',
-  height: '50vh',
-};
-const center = {
-  lat: 33.950001, // default latitude
-  lng: -83.383331, // default longitude
-};
 
 function App() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyC8f_v7OJAap8LH5VoSV8wtsgD4myaUvRI',
-    libraries,
-  });
-
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
-
-  if (!isLoaded) {
-    return <div>Loading maps</div>;
-  }
-
   return (
     <>
     <Router>
@@ -38,16 +14,6 @@ function App() {
         <Route path="/report" element={<Report />} />
       </Routes>
     </Router>
-
-    <div>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={center}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </div>
     </>
   );
 }
